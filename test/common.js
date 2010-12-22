6ff972313335b8521bbd0d0ab5edbe9302271443
Mike Bannister <mikebannister@gmail.com>
2010-12-21 20:54:42 -0500

    test dir cleanup made better

--- test/common.js
+++ test/common.js
@@ -13,15 +13,19 @@ var fakeHomeRoot = __dirname + '/files/fakeHome';
 
 var fsCount = 0;
 var fakeRoot;
+var fakeAdmin = 'mike';
 
 buildFakeFilesystem = exports.buildFakeFilesystem = function(fn) {
   fakeRoot = fakeHomeRoot + fsCount;
+  // this gets created by another process sometimes, deleting for safely
+  var deployDir = path.join(fakeHomeRoot, fakeAdmin, '.m1deploy');
+  var cleanDirs = [deployDir, fakeRoot];
   // delete destination first, just in case
-  rmdirAndChildren(fakeRoot, function() {
+  rmdirAndChildren(cleanDirs, function() {
     // make a copy
     copy(fakeHomeRoot, fakeRoot, function() {
       // point HOME env variable at fake home di
-      process.env.HOME = path.join(fakeRoot, 'mike');
+      process.env.HOME = path.join(fakeRoot, fakeAdmin);
       fsCount++;
       fn();
     });
